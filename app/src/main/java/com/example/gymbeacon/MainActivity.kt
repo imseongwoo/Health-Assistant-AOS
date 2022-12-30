085af29ea375953fd9cac6edd4d0a3019e6364b4
임성우 <seongwoorang@naver.com>
2022-12-30 16:44:43 +0900

    Add showToast func

--- app/src/main/java/com/example/gymbeacon/MainActivity.kt
+++ app/src/main/java/com/example/gymbeacon/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.gymbeacon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /* toast 메시지 출력 함수*/
+    fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
 }
