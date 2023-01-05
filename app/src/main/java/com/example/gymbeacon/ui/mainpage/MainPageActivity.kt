f52a711b843e6963f3832b1240940d1e5a86c48d
임성우 <seongwoorang@naver.com>
2023-01-05 12:03:45 +0900

    #4 [ui] Add ListAdapter

--- app/src/main/java/com/example/gymbeacon/ui/mainpage/MainPageActivity.kt
+++ app/src/main/java/com/example/gymbeacon/ui/mainpage/MainPageActivity.kt
@@ -2,11 +2,23 @@ package com.example.gymbeacon.ui.mainpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.gymbeacon.R
+import com.example.gymbeacon.databinding.ActivityMainPageBinding
 
 class MainPageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_page)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main_page)
+
+        val mainpageAdapter = MainpageAdapter()
+        binding.recyclerViewGymInfoList.adapter = mainpageAdapter
+
+        // GymInfoViewModel observe
     }
+
+
+
 }
