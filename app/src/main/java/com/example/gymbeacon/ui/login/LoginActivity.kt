97d0da99ea71962a0e4b8437a1ab8200d2efbb3d
임성우 <seongwoorang@naver.com>
2023-02-19 21:14:29 +0900

    #2 [ui] Add Sign Up Page

--- app/src/main/java/com/example/gymbeacon/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/gymbeacon/ui/login/LoginActivity.kt
@@ -1,12 +1,28 @@
 package com.example.gymbeacon.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.gymbeacon.R
+import com.example.gymbeacon.databinding.ActivityLoginBinding
+import com.example.gymbeacon.ui.signup.SignUpActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+
+        with(binding) {
+            buttonLogin.setOnClickListener {
+                goToSignUpActivity()
+            }
+        }
+    }
+
+    fun goToSignUpActivity() {
+        Intent(this,SignUpActivity::class.java).also { startActivity(it) }
     }
 }
