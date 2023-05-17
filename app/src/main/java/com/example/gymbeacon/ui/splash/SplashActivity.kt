6023eca7c87cb1036f755f9218a5a985eb4570f2
임성우 <seongwoorang@naver.com>
2023-05-17 09:42:26 +0900

    [ui] add splash ui

---
+++ app/src/main/java/com/example/gymbeacon/ui/splash/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.gymbeacon.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.gymbeacon.R
+import com.example.gymbeacon.ui.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent= Intent( this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 500)
+    }
+}
